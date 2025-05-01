e5c06083b54be7359f868e67e4cff14e739a03c0
Matias Canepa <macanepa@miuandes.cl>
2025-05-01 12:29:10 -0400

    chore: added autostart scripts

--- .config/qtile/autostart_once.sh
+++ .config/qtile/autostart_once.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
+
 # Apply wallpaper using wal
 wal -i ~/Wallpaper/claudio-testa-FrlCwXwbwkk-unsplash.jpg &&
 
 # Start picom
 picom --config ~/.config/picom/picom.conf &
 
+nm-applet &
 
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
+
+greenclip daemon &
+
+rclone --vfs-cache-mode writes mount OneDrive: ~/OneDrive & notify-send "OneDrive connected" "Microsoft OneDrive successfully mounted."
--- scripts/basic.sh
+++ scripts/basic.sh
@@ -1,5 +1,9 @@
-sudo pacman -S git vim ranger picom dunst rofi flameshot feh arandr thunar thunar-archive-plugin engrampa lxappearance gnome-keyring playerctl tmux rclone fuse3 man virt-viewer cameractrls xorg-xkill btop --noconfirm
+sudo pacman -S git vim ranger picom dunst rofi flameshot feh arandr thunar thunar-archive-plugin engrampa lxappearance gnome-keyring playerctl tmux rclone fuse3 man virt-viewer cameractrls xorg-xkill btop --noconfirm  --needed
 
-sudo pacman -S python-mpris2 python-dbus-next python-dbus-fast --noconfirm
+sudo pacman -S python-mpris2 python-dbus-next python-dbus-fast --noconfirm --needed
 
-#sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji --noconfirm
+
+if ! test -f ~/.config/rclone/rclone.conf; then
+	mkdir -p ~/OneDrive
+	rclone config
+fi
